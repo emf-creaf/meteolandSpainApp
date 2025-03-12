8d7f29a7f3c98a9398dcbf25b4b93b7ccd4ec24d
MalditoBarbudo <victorgrandagarcia@gmail.com>
2025-03-12 11:23:00 +0100

    app.R: added duckdb connection

---
+++ R/app.R
@@ -0,0 +1,25 @@
+#' App launcher
+#'
+#' Function to launch the app (in console, in container...)
+#'
+#' This function builds the UI (calling any modules necessary), the server
+#' (calling any modules necessary) and return a `ShinyApp()` object.
+#'
+#' @export
+meteoland_spain_app <- function() {
+  #### duckdb connection ####
+  duckdb_proxy <- duckdb::dbConnect(duckdb::duckdb())
+  withr::defer(duckdb::dbDisconnect(duckdb_proxy))
+  install_httpfs_statement <- glue_sql(
+    .con = duckdb_proxy,
+    "INSTALL httpfs;"
+  )
+  httpfs_statement <- glue_sql(
+    .con = duckdb_proxy,
+    "LOAD httpfs;"
+  )
+  dbExecute(duckdb_proxy, install_httpfs_statement)
+  dbExecute(duckdb_proxy, httpfs_statement)
+
+  
+}
